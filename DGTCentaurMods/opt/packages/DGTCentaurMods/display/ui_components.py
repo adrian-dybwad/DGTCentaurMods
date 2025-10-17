4d22a0a0d6be2120bf02b1d7fac385e821c797b8
Adrian Dybwad <adrian@purpleair.com>
2025-10-17 01:24:13 -0600

    Use packages in path

--- DGTCentaurMods/opt/packages/DGTCentaurMods/display/ui_components.py
+++ DGTCentaurMods/opt/packages/DGTCentaurMods/display/ui_components.py
@@ -38,5 +38,5 @@ class AssetManager():
         if os.path.exists("/home/pi/resources/" + resource_file):
             return "/home/pi/resources/" + resource_file
         else:
-            return "/opt/DGTCentaurMods/resources/" + resource_file
+            return "/opt/package/DGTCentaurMods/resources/" + resource_file
         
