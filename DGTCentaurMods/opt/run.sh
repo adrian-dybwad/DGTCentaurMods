ba6a6bf76fe9c5e131961cedf58fa33851ee26f4
Adrian Dybwad <adrian@purpleair.com>
2025-10-18 11:44:15 -0600

    Fix missing quote

--- DGTCentaurMods/opt/run.sh
+++ DGTCentaurMods/opt/run.sh
@@ -10,7 +10,7 @@ cd "$(dirname "$0")" || exit 1
 # Ensure virtualenv exists
 if [ ! -d "DGTCentaurMods/.venv" ]; then
   echo "No .venv found! Create one with:"
-  echo "   python3 -m venv --system-site-packages DGTCentaurMods/.venv && source DGTCentaurMods/.venv/bin/activate && pip install -r DGTCentaurMods>
+  echo "   python3 -m venv DGTCentaurMods/.venv && source DGTCentaurMods/.venv/bin/activate && pip install -r DGTCentaurMods/setup/requirements.txt"
   exit 1
 fi
 
