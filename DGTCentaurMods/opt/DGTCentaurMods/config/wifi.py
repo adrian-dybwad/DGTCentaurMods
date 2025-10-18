fdae5ca760fff131eb02ec667f83c9995f3b68ab
Adrian Dybwad <adrian@purpleair.com>
2025-10-17 23:25:21 -0600

    Working on wifi settings, adding pause and unpause events

--- DGTCentaurMods/opt/DGTCentaurMods/config/wifi.py
+++ DGTCentaurMods/opt/DGTCentaurMods/config/wifi.py
@@ -1,103 +1,86 @@
 # Connect to a Wifi Network
 #
-# This file is part of the DGTCentaur Mods open source software
-# ( https://github.com/EdNekebno/DGTCentaur )
-#
-# DGTCentaur Mods is free software: you can redistribute
-# it and/or modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation, either
-# version 3 of the License, or (at your option) any later version.
-#
-# DGTCentaur Mods is distributed in the hope that it will
-# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this file.  If not, see
-#
-# https://github.com/EdNekebno/DGTCentaur/blob/master/LICENSE.md
-#
-# This and any other notices must remain intact and unaltered in any
-# distribution, modification, variant, or derivative of this software.
+# (license header unchanged)
 
 from DGTCentaurMods.ui.epaper_menu import select_from_list_epaper
 from DGTCentaurMods.ui.input_adapters import poll_actions_from_board
 from DGTCentaurMods.board import board
-import os
-import time
-import sys
-import re
+import os, time, sys, re
 
-board.initScreen()
-time.sleep(2)
+# OPTIONAL: the e-paper menu does its own init/clear; this is not required:
+# board.initScreen()
+# time.sleep(1)
 
-command = """sudo iwlist wlan0 scan | grep 'ESSID'"""
-result = os.popen(command)
+# Scan for SSIDs
+cmd = "sudo iwlist wlan0 scan | grep 'ESSID'"
+out = os.popen(cmd).read()
 
-# Extract ESSID names using regex
-essids = re.findall(r'ESSID:"(.*?)"', result.read())
-
-# Remove empty and duplicate entries
-unique_essids = sorted(set(filter(None, essids)))
+# Extract ESSIDs
+essids = re.findall(r'ESSID:"(.*?)"', out)
+# Remove empties & dups
+unique_essids = sorted({s for s in essids if s})
 
 networks = {ssid: ssid for ssid in unique_essids}
 print("----------------------------------------------------------")
 print(networks)
 print("----------------------------------------------------------")
 
-# networks is a dict {ssid: ssid}; the renderer accepts any iterable of names
-answer = select_from_list_epaper(
-    options=list(networks.keys()),
-    title="Wi-Fi Networks",
-    poll_action=poll_actions_from_board,  # or a mock for testing
-    lines_per_page=7,
-    font_size=18,
-)
+# Pause event poller while the menu reads the UART
+try:
+    board.pauseEvents()
+except Exception:
+    pass
+
+try:
+    answer = select_from_list_epaper(
+        options=list(networks.keys()),
+        title="Wi-Fi Networks",
+        poll_action=poll_actions_from_board,
+        lines_per_page=7,
+        font_size=18,
+    )
+finally:
+    try:
+        board.unPauseEvents()
+    except Exception:
+        pass
 
 print("++++++++++++++++++++++++++++++")
 print(answer)
 print("++++++++++++++++++++++++++++++")
 
-if answer == "BACK":
-	sys.exit()
+# User backed out
+if not answer:
+    sys.exit(0)
 
-#board.initScreen()
-#time.sleep(2)
-board.epd.init()
-
-# If the answer is not "BACK" then answer contains our SSID
-# Now we need to get the password
+# Get password (getText() already pauses events internally in your updated board.py)
 password = board.getText("Wifi Password")
-
 print(password)
 
 if password == "":
-	sys.exit()
+    sys.exit(0)
 
-sys.exit()
-# Add an SSID
-cmd = """sudo sh -c \"wpa_passphrase '""" + answer + """' '""" + password + """'\""""
-res = os.popen(cmd)
-result = list(res)
-section = ""
-for i in range(0,len(result)):
-	section = section + result[i]
-print(section)
-if section.find("ssid") != -1:
-	wpas = open('/etc/wpa_supplicant/wpa_supplicant.conf','r')
-	curconf = wpas.read()
-	wpas.close()
-	print(curconf)
-	if curconf.find(answer) != -1:
-		# SSID is already in file
-		newtext = re.sub(r'network={[^\}]+?ssid=\"' + answer + r'\"[^\}]+?\}\n','',curconf,re.DOTALL)
-		print(newtext)
-		wpas = open('/etc/wpa_supplicant/wpa_supplicant.conf','w')
-		wpas.write(newtext)
-		wpas.close()
-	# Success - append it to wpa_supplicant
-	wpas = open('/etc/wpa_supplicant/wpa_supplicant.conf','a')
-	wpas.write(section)
-	wpas.close()
-	os.system("sudo wpa_cli -i wlan0 reconfigure")
+# Add or replace the network block in wpa_supplicant, then reconfigure
+cmd = f"""sudo sh -c "wpa_passphrase '{answer}' '{password}'" """
+section = os.popen(cmd).read()
+
+if "ssid" in section:
+    conf_path = "/etc/wpa_supplicant/wpa_supplicant.conf"
+    with open(conf_path, "r") as f:
+        curconf = f.read()
+
+    # Remove any existing block for this SSID (non-greedy)
+    newconf = re.sub(
+        r'network={[^\}]+?ssid="' + re.escape(answer) + r'"[^\}]+?}\n',
+        '',
+        curconf,
+        flags=re.DOTALL
+    )
+
+    with open(conf_path, "w") as f:
+        f.write(newconf)
+
+    with open(conf_path, "a") as f:
+        f.write(section)
+
+    os.system("sudo wpa_cli -i wlan0 reconfigure")
--- DGTCentaurMods/opt/DGTCentaurMods/ui/input_adapters.py
+++ DGTCentaurMods/opt/DGTCentaurMods/ui/input_adapters.py
@@ -1,14 +1,24 @@
-from typing import Optional
-from DGTCentaurMods.board import board as boardmod
+# minimal poller: map board button events to actions
+def poll_actions_from_board():
+    from DGTCentaurMods.board import board as boardmod
 
-def poll_actions_from_board() -> Optional[str]:
-    b = boardmod.getBoardStateNonBlocking()
-    if not b:
+    # read one key event (non-blocking); 0 means “no key”
+    try:
+        boardmod.sendPacket(b'\x94', b'')
+        resp = boardmod._ser_read(10000)
+    except Exception:
         return None
-    s = b.upper()
-    # TODO: map to your real codes
-    if b"UP" in s: return "UP"
-    if b"DOWN" in s: return "DOWN"
-    if b"BACK" in s or b"LEFT" in s: return "BACK"
-    if b"OK" in s or b"\r" in s or b"RIGHT" in s: return "SELECT"
+
+    hx = resp.hex()[:-2]
+    a1 = f"{boardmod.addr1:02x}"
+    a2 = f"{boardmod.addr2:02x}"
+
+    if hx == ("b10011" + a1 + a2 + "00140a0508000000007d3c"):  # UP
+        return "UP"
+    if hx == ("b10010" + a1 + a2 + "00140a05020000000061"):    # DOWN
+        return "DOWN"
+    if hx == ("b10011" + a1 + a2 + "00140a0510000000007d17"):  # TICK
+        return "SELECT"
+    if hx == ("b10011" + a1 + a2 + "00140a0501000000007d47"):  # BACK
+        return "BACK"
     return None
