68307f3136e2221963c374300b38b2f2d8f1f14b
Adrian Dybwad <adrian@purpleair.com>
2025-10-19 13:13:30 -0600

    Remove default nginx site

--- DGTCentaurMods/opt/DGTCentaurMods/scripts/enable-web-proxy.sh
+++ DGTCentaurMods/opt/DGTCentaurMods/scripts/enable-web-proxy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Enable nginx site for centaurmods-web
+rm -f /etc/nginx/sites-enabled/default
 ln -sf /etc/nginx/sites-available/centaurmods-web /etc/nginx/sites-enabled/
 nginx -t && systemctl reload nginx
