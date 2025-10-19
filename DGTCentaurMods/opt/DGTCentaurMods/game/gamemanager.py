375abcf1736b3f1126eb908085510d813c0c8758
Adrian Dybwad <adrian@purpleair.com>
2025-10-19 15:38:26 -0600

    Revert piece placed vs lifted logic

--- DGTCentaurMods/opt/DGTCentaurMods/game/gamemanager.py
+++ DGTCentaurMods/opt/DGTCentaurMods/game/gamemanager.py
@@ -147,8 +147,9 @@ def keycallback(keypressed):
             inmenu = 0
 
 def fieldcallback(field):
-    # Receives field events. Positive is a field place, negative is a field lift. Numbering 1-64 (1 = a1, 64 = h8)
+    # Receives field events. Positive is a field lift, negative is a field place. Numbering 1-64 (1 = a1, 64 = h8)
     # Use this to calculate moves
+    # Note: This appears backwards but reverses the corrected parseFieldEvents() output back to what the game logic expects
     global cboard
     global curturn
     global movecallbackfunction
@@ -165,11 +166,11 @@ def fieldcallback(field):
     global showingpromotion
     lift = 0
     place = 0
-    if field < 0:
+    if field >= 0:
         lift = 1
-        field = field * -1  # Make positive for indexing
     else:
         place = 1
+        field = field * -1
     field = field - 1
     # Check the piece colour against the current turn
     pc = cboard.color_at(field)
