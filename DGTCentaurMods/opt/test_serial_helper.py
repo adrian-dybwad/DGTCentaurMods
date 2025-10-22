7ad920943c8f0ea13a12f41f3365ed9dd5c9e0dd
Adrian Dybwad <adrian@purpleair.com>
2025-10-21 21:41:27 -0600

    Update test_serial_helper.py

--- DGTCentaurMods/opt/test_serial_helper.py
+++ DGTCentaurMods/opt/test_serial_helper.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
         try:
             helper.ledsOff()
             #helper.sendPacket(b'\x83', b'') #Piece detection enabled
-            print("\nListening for serial data (press Ctrl+C to stop)...")
             helper.ledsOff()
         except Exception as e:
             print(f"Error: {e}")
