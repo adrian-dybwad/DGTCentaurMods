8688234bf7647ad2c0a5bd102eaf77a0ccea675a
Adrian Dybwad <adrian@purpleair.com>
2025-10-22 17:33:16 -0600

    Update async_serial.py

--- DGTCentaurMods/opt/DGTCentaurMods/board/async_serial.py
+++ DGTCentaurMods/opt/DGTCentaurMods/board/async_serial.py
@@ -180,9 +180,12 @@ class AsyncSerial:
         self.listener_thread.start()
 
         # THEN send discovery commands
+        print("Sending discovery commands")
         self._discover_board_address()
+        print(f"start_key_polling: {start_key_polling}")
         if start_key_polling:
             self.sendPacket(KEY_POLL_CMD, b'')
+        print("Key polling started")
         
     def wait_ready(self, timeout=60):
         """
