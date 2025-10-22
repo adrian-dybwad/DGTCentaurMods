9442063b660b09428719f7e06302516c863bf19f
Adrian Dybwad <adrian@purpleair.com>
2025-10-21 20:37:58 -0600

    Switch to getting buttons singing

--- DGTCentaurMods/opt/test_serial_helper.py
+++ DGTCentaurMods/opt/test_serial_helper.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     if helper.wait_ready():
         
         helper.ledsOff()
-        helper.sendPacket(b'\x83', b'') #Piece detection enabled
-        #helper.sendPacket(b'\x94', b'') #Key detection enabled
+        #helper.sendPacket(b'\x83', b'') #Piece detection enabled
+        helper.sendPacket(b'\x94', b'') #Key detection enabled
         print("\nListening for serial data (press Ctrl+C to stop)...")
         try:
             while True:
