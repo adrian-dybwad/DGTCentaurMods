1174bf34ba58e326acc408cb6d9f0922b7fc0b0c
Adrian Dybwad <adrian@purpleair.com>
2025-10-23 13:47:37 -0600

    Update board.py

--- DGTCentaurMods/opt/DGTCentaurMods/board/board.py
+++ DGTCentaurMods/opt/DGTCentaurMods/board/board.py
@@ -512,7 +512,6 @@ def getBoardState(field=None, retries=6, sleep_between=0.12):
                 tval = (payload[i] << 8) | payload[i+1]
                 boarddata[i // 2] = 1 if (lowerlimit <= tval <= upperlimit) else 0
 
-            printBoardState()
             if field is not None:
                 return boarddata[field]
             return boarddata
