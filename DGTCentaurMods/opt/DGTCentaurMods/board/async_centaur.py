facf7fe5ddeed74f58dc5bcf52fa638a24fa0c43
Adrian Dybwad <adrian@purpleair.com>
2025-10-23 19:33:10 -0600

    Update async_centaur.py

--- DGTCentaurMods/opt/DGTCentaurMods/board/async_centaur.py
+++ DGTCentaurMods/opt/DGTCentaurMods/board/async_centaur.py
@@ -953,7 +953,11 @@ class AsyncCentaur:
             print("Discovery: sending discovery packet 0x87 0x00 0x00 0x07")
             #tosend = bytearray(DGT_DISCOVERY_ACK)
             #self.ser.write(tosend)
-            self.sendPacket(DGT_DISCOVERY_ACK)
+            if len(packet) == 5 and packet[0] == 0x93:
+                print(f"Discovery: ACK received")
+                # Also clear parser buffer so header detection won't prepend stale bytes
+                self.response_buffer = bytearray()
+                self.sendPacket(DGT_DISCOVERY_ACK)
         
         elif self.discovery_state == "AWAITING_PACKET":
             if len(packet) > 4:
