3226c33ba50eea27a62ebcc0c914db83ad84eb4c
Adrian Dybwad <adrian@purpleair.com>
2025-10-24 02:11:40 -0600

    Update uci.py

--- DGTCentaurMods/opt/DGTCentaurMods/uci.py
+++ DGTCentaurMods/opt/DGTCentaurMods/uci.py
@@ -67,7 +67,7 @@ print("ENGINE_PATH_NAME: " + ENGINE_PATH_NAME)
 CT_800_PATH = "engines/ct800"
 print("CT_800_PATH: " + CT_800_PATH)
 AENGINE_PATH = str(pathlib.Path(__file__).parent / CT_800_PATH).resolve() 
-PENGINE_PATH = str(pathlib.Path(__file__).parent / ENGINE_PATH_NAME).resolve())
+PENGINE_PATH = str(pathlib.Path(__file__).parent / ENGINE_PATH_NAME).resolve()
 UCI_FILE_PATH = PENGINE_PATH + ".uci"
 print(f"aengine: {AENGINE_PATH}")
 print(f"pengine: {PENGINE_PATH}")
