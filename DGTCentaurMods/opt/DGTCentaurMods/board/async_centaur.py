48308355bf11b91da6f70d8d5422bb325dd0e31b
Adrian Dybwad <adrian@purpleair.com>
2025-10-23 18:42:21 -0600

    Update async_centaur.py

--- DGTCentaurMods/opt/DGTCentaurMods/board/async_centaur.py
+++ DGTCentaurMods/opt/DGTCentaurMods/board/async_centaur.py
@@ -356,7 +356,10 @@ class AsyncCentaur:
         # Detect packet start sequence (<START_TYPE_BYTE> 00) while buffer has data
         HEADER_DATA_BYTES = 4
         if len(self.response_buffer) >= HEADER_DATA_BYTES:
-            print(f"response_buffer longer than HEADER_DATA_BYTES: {HEADER_DATA_BYTES}")
+            print(f"\n\n\nresponse_buffer longer than HEADER_DATA_BYTES: {HEADER_DATA_BYTES}")
+            print(f"self.response_buffer[-HEADER_DATA_BYTES]: {self.response_buffer[-HEADER_DATA_BYTES]}")
+            print(f"IN START_TYPE_BYTES: {START_TYPE_BYTES}")
+            print(f"self.response_buffer[-HEADER_DATA_BYTES] in START_TYPE_BYTES: {self.response_buffer[-HEADER_DATA_BYTES] in START_TYPE_BYTES}")
             if self.response_buffer[-HEADER_DATA_BYTES] in START_TYPE_BYTES:
                 print(f"IN START_TYPE_BYTES: {START_TYPE_BYTES}")
                 print(f"self.response_buffer[-HEADER_DATA_BYTES]: {self.response_buffer[-HEADER_DATA_BYTES]}")
