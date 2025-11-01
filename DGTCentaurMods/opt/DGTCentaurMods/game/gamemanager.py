a3265980a57c5165e814ac239751877383cd343a
Adrian Dybwad <adrian@purpleair.com>
2025-11-01 10:50:49 -0600

    Update gamemanager.py

--- DGTCentaurMods/opt/DGTCentaurMods/game/gamemanager.py
+++ DGTCentaurMods/opt/DGTCentaurMods/game/gamemanager.py
@@ -490,9 +490,9 @@ def keycallback(key_pressed):
 def fieldcallback(piece_event, field, time_in_seconds):
     # Receives field events. Positive is a field lift, negative is a field place. Numbering 0 = a1, 63 = h8
     # Use this to calculate moves
-    field = square + 1 # Convert to positive field number
-    if piece_event == 1: # PLACE
-        field = ((square + 1) * -1) # Convert to negative field number
+    # field = square + 1 # Convert to positive field number
+    # if piece_event == 1: # PLACE
+    #     field = ((square + 1) * -1) # Convert to negative field number
 
     print(f"[gamemanager.fieldcallback] piece_event={piece_event} field_hex={field} field={field} time_in_seconds={time_in_seconds}")
     global cboard
