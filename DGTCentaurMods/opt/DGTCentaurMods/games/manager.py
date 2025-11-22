e2b39beafb5e3b060ab1d8be0b85f52606ae49c8
Adrian Dybwad <adrian@purpleair.com>
2025-11-22 12:55:18 -0700

    Update manager.py

--- DGTCentaurMods/opt/DGTCentaurMods/games/manager.py
+++ DGTCentaurMods/opt/DGTCentaurMods/games/manager.py
@@ -460,7 +460,7 @@ class GameManager:
             elif len(extra_squares) > 0:
                 board.ledsOff()
                 # Use ledArray for continuous flashing instead of single flash
-                board.ledArray(extra_squares, speed=3, intensity=5)
+                board.ledArray(extra_squares, speed=5, intensity=5)
                 log.warning(f"[GameManager._provide_correction_guidance] Extra pieces at: {[chess.square_name(sq) for sq in extra_squares]}")
     
     def _handle_field_event_in_correction_mode(self, piece_event: int, field: int, time_in_seconds: float):
