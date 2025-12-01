32f53049f3e9d7dde0a5b6cbd2b46f3546480855
Adrian Dybwad <adrian@purpleair.com>
2025-12-01 12:48:52 -0700

    Update pegasus.py

--- DGTCentaurMods/opt/DGTCentaurMods/games/pegasus.py
+++ DGTCentaurMods/opt/DGTCentaurMods/games/pegasus.py
@@ -106,9 +106,10 @@ class Pegasus:
             field: Chess field index (0-63)
             time_in_seconds: Time in seconds since the start of the game
         """
-        log.info(f"[Pegasus] handle_manager_event called: event={event} piece_event={piece_event}, field={field}, time_in_seconds={time_in_seconds}")
+        field_hex = board.rotateFieldHex(field)
+        log.info(f"[Pegasus] handle_manager_event called: event={event} piece_event={piece_event}, field={field}, field_hex={field_hex}, time_in_seconds={time_in_seconds}")
         if event == EVENT_LIFT_PIECE or event == EVENT_PLACE_PIECE:
-            self.send_packet(FIELD_UPDATE_RESP, [field, piece_event])
+            self.send_packet(FIELD_UPDATE_RESP, [field_hex, piece_event])
     
     def handle_manager_move(self, move):
         """Handle moves from the manager.
